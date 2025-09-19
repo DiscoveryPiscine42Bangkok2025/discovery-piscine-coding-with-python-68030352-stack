e0dd27cfbe26904a6dd8fbb8da11567e600799b0
68030352-stack <68030352@kmitl.ac.th>
2025-09-19 01:24:26 +0000

    5.1

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,3 +1,8 @@
 original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-filtered = [num for num in original_array if num > 5]
-print(filtered)
+
+new_array = []
+for num in original_array:
+    new_array.append(num + 2)
+
+print("Original array:", original_array)
+print("New array:", new_array)
