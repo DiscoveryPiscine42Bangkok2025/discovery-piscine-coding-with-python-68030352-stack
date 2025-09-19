8c3ece54703e578b0658a5f4793cad9511469412
68030352-stack <68030352@kmitl.ac.th>
2025-09-19 03:08:43 +0000

    5.2

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,11 +1,9 @@
-original_array = [2, 8, 9, 48, 8, 22, -12, 2]
-#filter values greater than 5
-filtered = [num for num in original_array if num > 5]
-#Remove duplicates using a set while preserving order
-unique_filtered = []
-seen = set()
-for num in filtered:
-    if num not in seen:
-        unique_filtered.append(num)
-        seen.add(num)
-print(unique_filtered)
+def main():
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    new_array = [x + 2 for x in original_array if x > 5] # +2 ให้ตัวที่มากกว่า 5
+
+    print(original_array)
+    print(new_array)
+
+if __name__ == "__main__":
+    main()
